f336bc33f993daddc470874ba343f59e007e5683
sanketghongade08 <sanketghongade08@gmail.com>
2026-08-06 17:23:46 +0000

    new

--- use.cpp
+++ use.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // Header file for input/output operations
 using namespace std; // Use standard namespace
 int main() {
-   cout << "Hello, World!"; // Output statement
+   cout << "Hello, github!"; // Output statement
    return 0; // Exit status
 }
