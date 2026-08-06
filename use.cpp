c9167d882a3583fccd56b2c46bf05d453e586a1f
sanketghongade08 <sanketghongade08@gmail.com>
2026-08-06 17:14:51 +0000

    first c++

---
+++ use.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Header file for input/output operations
+using namespace std; // Use standard namespace
+int main() {
+   cout << "Hello, World!"; // Output statement
+   return 0; // Exit status
+}
